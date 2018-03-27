be71e781485a920621e266dde051874c8fe9e9e9
succcubbus <jannis.kaiser2@gmail.com>
2018-03-27 21:15:47 +0200

    apply elixir formatter to mix.exs

--- mix.exs
+++ mix.exs
@@ -2,28 +2,30 @@ defmodule Lifx.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :lifx,
-     version: "0.1.7",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     description: description(),
-     package: package(),
-     deps: deps()]
+    [
+      app: :lifx,
+      version: "0.1.7",
+      elixir: "~> 1.3",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
+      deps: deps()
+    ]
   end
 
   def application do
     [
-        applications: [:logger, :cowboy, :poison],
-        mod: {Lifx, []},
-        env: [tcp_server: false, tcp_port: 8800]
+      applications: [:logger, :cowboy, :poison],
+      mod: {Lifx, []},
+      env: [tcp_server: false, tcp_port: 8800]
     ]
   end
 
   def description do
-      """
-      A Client for Lifx LAN API
-      """
+    """
+    A Client for Lifx LAN API
+    """
   end
 
   def package do
@@ -32,16 +34,18 @@ defmodule Lifx.Mixfile do
       files: ["lib", "priv", "mix.exs", "README*", "LICENSE*"],
       maintainers: ["Christopher Steven Coté"],
       licenses: ["Apache License 2.0"],
-      links: %{"GitHub" => "https://github.com/NationalAssociationOfRealtors/lifx",
-          "Docs" => "https://github.com/NationalAssociationOfRealtors/lifx"}
+      links: %{
+        "GitHub" => "https://github.com/NationalAssociationOfRealtors/lifx",
+        "Docs" => "https://github.com/NationalAssociationOfRealtors/lifx"
+      }
     ]
   end
 
   defp deps do
     [
-        {:cowboy, "~> 2.2"},
-        {:poison, "~> 3.1"},
-        {:ex_doc, ">= 0.0.0", only: :dev}
+      {:cowboy, "~> 2.2"},
+      {:poison, "~> 3.1"},
+      {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
 end
